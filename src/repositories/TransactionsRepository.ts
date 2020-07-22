02c8048980bd369858e51332f4646103a65e206a
Roadm11 <rodrigo.adm10@hotmail.com>
2020-07-21 23:15:28 -0300

    Upload challenge completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const transactionIncome = this.transactions.reduce((total, income) => {
+      if (income.type.toLowerCase() === 'income') {
+        return total + income.value;
+      }
+      return total;
+    }, 0);
+
+    const transactionOutcome = this.transactions.reduce((total, outcome) => {
+      if (outcome.type.toLowerCase() === 'outcome') {
+        return total + outcome.value;
+      }
+      return total;
+    }, 0);
+
+    const total = Math.round(transactionIncome - transactionOutcome);
+
+    const balance: Balance = {
+      income: transactionIncome,
+      outcome: transactionOutcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,32 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (value < 0) {
+      throw Error(
+        'Value cant be a negative number, change the type to "outcome"',
+      );
+    }
+
+    if (type.toLowerCase() !== 'income' && type.toLowerCase() !== 'outcome') {
+      throw Error('The type just can be a income or outcome');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome') {
+      if (total > 0 && total < value) {
+        throw Error('Transaction denied. The value is bigger than your total');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
